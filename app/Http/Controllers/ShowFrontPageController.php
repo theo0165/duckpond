f8f6dacfd19634e33a0bd764090481da5cb69781
Theo Sandell <theo01sandell@gmail.com>
2022-03-08 14:43:27 +0100

    Fixed fallback if user is not logged in

--- app/Http/Controllers/ShowFrontPageController.php
+++ app/Http/Controllers/ShowFrontPageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class ShowFrontPageController extends Controller
@@ -12,11 +13,26 @@ class ShowFrontPageController extends Controller
     {
         // Get first 100 posts, sorted by most upvoted.
         $user = auth()->user();
-        $posts = $user->frontPagePosts;
+
+        if($user){
+            $posts = $user->frontPagePosts;
+        }else{
+            $posts = $this->getGuestPosts();
+        }
 
         return view('frontpage', [
             'user' => $user,
             'posts' => $posts
         ]);
+    }
+
+    private function getGuestPosts()
+    {
+        return Post::with(['community', 'user'])
+                ->where('posts.created_at', '>=', Carbon::now()->subDay()->toDateTimeString())
+                ->withSum('votes as votes', 'value')
+                ->limit(100)
+                ->orderBy('votes', 'desc')
+                ->get();
     }
 }
