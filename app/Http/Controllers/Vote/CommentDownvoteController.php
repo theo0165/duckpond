f2a4823d3aabe319db87c798ef7bd02ef3c76815
patrikstaaf <staaf.patrik@gmail.com>
2022-03-10 15:19:15 +0100

    Time for some controller clean up..

---
+++ app/Http/Controllers/Vote/CommentDownvoteController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Vote;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CommentDownvoteController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+    }
+}
