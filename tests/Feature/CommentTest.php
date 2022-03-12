89a3b78c44ae457b89571805a85bf5e0e359dc91
Theo Sandell <theo01sandell@gmail.com>
2022-03-12 17:53:46 +0100

    Add empty comment test functions. Over 50 tests 🤯

--- tests/Feature/CommentTest.php
+++ tests/Feature/CommentTest.php
@@ -9,4 +9,18 @@ use Tests\TestCase;
 class CommentTest extends TestCase
 {
     use RefreshDatabase;
+
+    public function test_user_can_create_comment(){}
+
+    public function test_user_can_reply_to_comment(){}
+
+    public function test_user_can_see_post_with_comments_and_replys(){}
+
+    public function test_user_can_upvote_comment(){}
+
+    public function test_user_can_downvote_comment(){}
+
+    public function test_user_can_delete_own_comment(){}
+
+    public function test_user_can_not_delete_other_users_comment(){}
 }
