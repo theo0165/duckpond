654d5f509eed33e3d319b1b45d07210772ced6aa
Theo Sandell <theo01sandell@gmail.com>
2022-03-12 17:43:44 +0100

    Add empty user test functions

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -9,4 +9,14 @@ use Tests\TestCase;
 class PostTest extends TestCase
 {
     use RefreshDatabase;
+
+    public function test_can_see_single_post(){}
+
+    public function test_user_can_create_post(){}
+
+    public function test_user_can_be_delete_post(){}
+
+    public function test_user_can_upvote_post(){}
+
+    public function test_user_can_downvote_post(){}
 }
