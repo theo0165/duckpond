0992ff56495e431bb7d0df197694e2d43947a450
Theo Sandell <theo01sandell@gmail.com>
2022-03-12 21:23:18 +0100

    Add user can create comment and reply test

--- tests/Feature/CommentTest.php
+++ tests/Feature/CommentTest.php
@@ -2,6 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Models\Comment;
+use App\Models\Community;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\UserFollowsCommunity;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -10,9 +15,50 @@ class CommentTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_user_can_create_comment(){}
+    public function test_user_can_create_comment(){
+        $user = User::factory()->create();
+        $community = Community::factory()->create();
+        $post = Post::factory()->text_type()->create();
+        UserFollowsCommunity::factory()->create();
 
-    public function test_user_can_reply_to_comment(){}
+        $request = $this
+                    ->followingRedirects()
+                    ->actingAs($user)
+                    ->post("/c/{$community->title}/p/{$post->getHashId()}/comment/create", [
+                        'content' => 'Test comment'
+                    ]);
+
+        $request->assertOk();
+        $this->assertDatabaseHas('comments', [
+            'user_id' => $user->id,
+            'parent_id' => null,
+            'post_id' => $post->id,
+            'content' => 'Test comment'
+        ]);
+    }
+
+    public function test_user_can_reply_to_comment(){
+        $user = User::factory()->create();
+        $community = Community::factory()->create();
+        $post = Post::factory()->text_type()->create();
+        $comment = Comment::factory()->create();
+        UserFollowsCommunity::factory()->create();
+
+        $request = $this
+                    ->followingRedirects()
+                    ->actingAs($user)
+                    ->post("/c/{$community->title}/p/{$post->getHashId()}/comment/{$comment->getHashId()}/create", [
+                        'content' => 'Test comment'
+                    ]);
+
+        $request->assertOk();
+        $this->assertDatabaseHas('comments', [
+            'user_id' => $user->id,
+            'parent_id' => $comment->id,
+            'post_id' => null,
+            'content' => 'Test comment'
+        ]);
+    }
 
     public function test_user_can_see_post_with_comments_and_replys(){}
 
