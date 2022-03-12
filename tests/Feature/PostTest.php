3c3076c428f14cef75b063a9ebe2944f591e9332
Theo Sandell <theo01sandell@gmail.com>
2022-03-12 22:35:32 +0100

    Add user can upvote and downvote post tests

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -54,11 +54,45 @@ class PostTest extends TestCase
 
     public function test_user_can_delete_post(){}
 
-    public function test_user_can_upvote_post(){}
+    public function test_user_can_upvote_post(){
+        $user = User::factory()->create();
+        $community = Community::factory()->create();
+        $post = Post::factory()->text_type()->create();
+
+        $request = $this
+                    ->followingRedirects()
+                    ->actingAs($user)
+                    ->get("/c/{$community->title}/p/{$post->getHashId()}/upvote");
+
+        $request->assertOk();
+        $this->assertDatabaseHas('votes', [
+            'user_id' => $user->id,
+            'post_id' => $post->id,
+            'comment_id' => null,
+            'value' => 1
+        ]);
+    }
 
     public function test_user_can_not_double_upvote_post(){}
 
-    public function test_user_can_downvote_post(){}
+    public function test_user_can_downvote_post(){
+        $user = User::factory()->create();
+        $community = Community::factory()->create();
+        $post = Post::factory()->text_type()->create();
+
+        $request = $this
+                    ->followingRedirects()
+                    ->actingAs($user)
+                    ->get("/c/{$community->title}/p/{$post->getHashId()}/downvote");
+
+        $request->assertOk();
+        $this->assertDatabaseHas('votes', [
+            'user_id' => $user->id,
+            'post_id' => $post->id,
+            'comment_id' => null,
+            'value' => -1
+        ]);
+    }
 
     public function test_user_can_not_double_downvote_post(){}
 }
