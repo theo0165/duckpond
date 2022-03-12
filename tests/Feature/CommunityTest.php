87bad8d33092739c6fc6fa093bedcb46179dafdc
Theo Sandell <theo01sandell@gmail.com>
2022-03-12 17:51:10 +0100

    Add empty community test functions

--- tests/Feature/CommunityTest.php
+++ tests/Feature/CommunityTest.php
@@ -9,4 +9,22 @@ use Tests\TestCase;
 class CommunityTest extends TestCase
 {
     use RefreshDatabase;
+
+    public function test_user_can_see_single_community(){}
+
+    public function test_user_can_follow_community(){}
+
+    public function test_user_can_not_follow_if_already_following(){}
+
+    public function test_user_can_unfollow_community(){}
+
+    public function test_user_can_not_unfollow_if_not_following(){}
+
+    public function test_user_can_create_community(){}
+
+    public function test_user_can_delete_owned_community(){}
+
+    public function test_user_can_not_delete_other_users_community(){}
+
+    public function test_user_can_see_page_with_all_communities(){}
 }
