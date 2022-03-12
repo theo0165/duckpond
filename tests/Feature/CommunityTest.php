4a69e38e38875d6b6ec2514cf2f723b83be8dd44
Theo Sandell <theo01sandell@gmail.com>
2022-03-12 18:16:24 +0100

    Add community follow/unfollow tests

--- tests/Feature/CommunityTest.php
+++ tests/Feature/CommunityTest.php
@@ -2,6 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\Models\Community;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\UserFollowsCommunity;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -10,15 +14,84 @@ class CommunityTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_user_can_see_single_community(){}
+    public function test_user_can_see_single_community(){
+        $user = User::factory()->create();
+        $community = Community::factory()->create();
+        $post = Post::factory()->text_type()->create();
 
-    public function test_user_can_follow_community(){}
+        $request = $this
+                    ->actingAs($user)
+                    ->followingRedirects()
+                    ->get("/c/{$community->title}");
 
-    public function test_user_can_not_follow_if_already_following(){}
+        $request->assertOk();
+        $request->assertSeeText([
+            "/c/{$community->title}",
+            $post->title,
+        ]);
+    }
 
-    public function test_user_can_unfollow_community(){}
+    public function test_user_can_follow_community(){
+        $user = User::factory()->create();
+        $community = Community::factory()->create();
+        $post = Post::factory()->text_type()->create();
 
-    public function test_user_can_not_unfollow_if_not_following(){}
+        $request = $this
+                    ->actingAs($user)
+                    ->followingRedirects()
+                    ->post("/c/{$community->title}/follow");
+
+        $request->assertOk();
+        $this->assertDatabaseHas('user_follows_communities', [
+            'user_id' => $user->id,
+            'community_id' => $community->id
+        ]);
+    }
+
+    public function test_user_can_not_follow_if_already_following(){
+        $user = User::factory()->create();
+        $community = Community::factory()->create();
+        $post = Post::factory()->text_type()->create();
+
+        UserFollowsCommunity::factory()->create();
+
+        $request = $this
+                    ->actingAs($user)
+                    ->followingRedirects()
+                    ->post("/c/{$community->title}/follow");
+
+        $request->assertOk();
+        $this->assertDatabaseCount('user_follows_communities', 1);
+    }
+
+    public function test_user_can_unfollow_community(){
+        $user = User::factory()->create();
+        $community = Community::factory()->create();
+        $post = Post::factory()->text_type()->create();
+
+        UserFollowsCommunity::factory()->create();
+
+        $request = $this
+                    ->actingAs($user)
+                    ->followingRedirects()
+                    ->post("/c/{$community->title}/unfollow");
+
+        $request->assertOk();
+        $this->assertDatabaseCount('user_follows_communities', 0);
+    }
+
+    public function test_user_can_not_unfollow_if_not_following(){
+        $user = User::factory()->create();
+        $community = Community::factory()->create();
+        $post = Post::factory()->text_type()->create();
+
+        $request = $this
+                    ->actingAs($user)
+                    ->followingRedirects()
+                    ->post("/c/{$community->title}/unfollow");
+
+        $request->assertOk();
+    }
 
     public function test_user_can_create_community(){}
 
