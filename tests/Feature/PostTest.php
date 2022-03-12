0389d537f515c0ef5dd09ac6def76c2d05f7bd73
Theo Sandell <theo01sandell@gmail.com>
2022-03-12 22:15:38 +0100

    Add can see single post test

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Models\Community;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -10,7 +13,17 @@ class PostTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_can_see_single_post(){}
+    public function test_can_see_single_post(){
+        $user = User::factory()->create();
+        $community = Community::factory()->create();
+        $post = Post::factory()->text_type()->create();
+
+        $request = $this
+                    ->get("/c/{$community->title}/p/{$post->getHashId()}");
+
+        $request->assertOk();
+        $request->assertSeeText($post->title);
+    }
 
     public function test_user_can_create_post(){}
 
