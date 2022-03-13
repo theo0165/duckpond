23d43c83123022a0b6f9e4782cfaa524669eba52
patrikstaaf <staaf.patrik@gmail.com>
2022-03-13 20:14:18 +0100

    Display follow/unfollow button if.. Not working, will have a look at it again later or tomorrow

--- app/Http/Controllers/Community/ShowCommunityController.php
+++ app/Http/Controllers/Community/ShowCommunityController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Community;
 
 use App\Http\Controllers\Controller;
 use App\Models\Community;
+use App\Models\UserFollowsCommunity;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
@@ -13,12 +14,17 @@ class ShowCommunityController extends Controller
     public function __invoke(Request $request, Community $community)
     {
         $user = auth()->user() ?: null;
+        // $user = auth()->user();
         $posts = $community->getPosts();
+
+        // $checkFollows = UserFollowsCommunity::where('user_id', $user->id)->where('community_id', $community->id)->get();
+        // dd($checkFollows);
 
         return view('community.show', [
             'user' => $user,
             'posts' => $posts,
-            'community' => $community
+            'community' => $community,
+            // 'checkFollows' => $checkFollows,
         ]);
     }
 }
