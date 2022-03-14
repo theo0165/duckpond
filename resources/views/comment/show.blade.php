23b053352716a9b2a095886b6356a234ca046f40
Theo Sandell <theo01sandell@gmail.com>
2022-03-14 17:14:20 +0100

    Preview parent comment when replying

--- resources/views/comment/show.blade.php
+++ resources/views/comment/show.blade.php
@@ -1,9 +1,18 @@
 <x-layout>
     <h1>Create reply</h1>
+    <p>Parent</p>
+    <ul>
+        <li>
+            <a href="{{ route('users.profile', $comment->owner) }}"><b>/u/{{$comment->owner->username}}</b></a>
+            <p>{{$comment->content}}</p>
+        </li>
+    </ul>
     <form action="{{ route('comment.reply.create', ['community' => $community, 'comment' => $comment->getHashId(), 'post' => $post->getHashId()]) }}" method="post">
         @csrf
-        <label for="content">Comment:</label>
-        <textarea name="content" id="content" cols="30" rows="10"></textarea>
-        <button type="submit">Submit</button>
+        <div>
+            <label for="content" class="form-label">Comment:</label>
+            <textarea name="content" id="content" cols="30" rows="10" class="form-control"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary mt-3">Submit</button>
     </form>
 </x-layout>
