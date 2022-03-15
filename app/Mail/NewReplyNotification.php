f5990d1142e09b37d837e86f79a5853a7d4c3942
Theo Sandell <theo01sandell@gmail.com>
2022-03-15 13:41:03 +0100

    Update NewReplyNotification.php

--- app/Mail/NewReplyNotification.php
+++ app/Mail/NewReplyNotification.php
@@ -11,7 +11,8 @@ use Illuminate\Queue\SerializesModels;
 
 class NewReplyNotification extends Mailable
 {
-    use Queueable, SerializesModels;
+    use Queueable;
+    use SerializesModels;
 
     /**
      * Create a new message instance.
