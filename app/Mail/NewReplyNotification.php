1e340d3bc5155e911e38020e2d98404ecbba3e2f
Theo Sandell <theo01sandell@gmail.com>
2022-03-15 13:02:13 +0100

    Mail notification when replying to post or comment

---
+++ app/Mail/NewReplyNotification.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Community;
+use App\Models\Post;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class NewReplyNotification extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Community $community, Post $post, string $type)
+    {
+        $this->community = $community;
+        $this->post = $post;
+        $this->type = $type;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+                ->subject('New reply')
+                ->view('mail.new-reply-notification', [
+                    'community' => $this->community,
+                    'post' => $this->post,
+                    'type' => $this->type
+                ]);
+    }
+}
